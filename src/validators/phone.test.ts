b7df5e7d2e6d358b0057dbd481f4e67a26323b95
Bruno Vinicius <vruno.binicius@gmail.com>
2022-03-17 23:09:20 -0300

    update phone test

--- src/validators/phone.test.ts
+++ src/validators/phone.test.ts
@@ -1,8 +1,8 @@
 import { isValidPhone, formatPhone } from "./phone";
 
 test('Validate phone by string', ()=>{
-    let validPhoneStr:string = '87981615939'
-    let otherValidPhoneStr:string = '(87) 9 8161-5939'
+    let validPhoneStr:string = '87956545351'
+    let otherValidPhoneStr:string = '(87) 9 1453-7533'
     let invalidPhoneStr:string = '846826 064 9'
 
     expect(isValidPhone(validPhoneStr)).toBe(true)
@@ -11,9 +11,9 @@ test('Validate phone by string', ()=>{
 })
 
 test('Remove invalid chars from valid phone', ()=>{
-    let validPhoneStr = '(87) 9 8161-5939'
-    let otherValidPhoneStr = '+55 (87) 9 8161-5939'
+    let validPhoneStr = '(87) 9 7689-5831'
+    let otherValidPhoneStr = '+55 (87) 9 5313-5930'
 
-    expect(formatPhone(validPhoneStr)).toBe('558781615939@c.us')
-    expect(formatPhone(otherValidPhoneStr)).toBe('558781615939@c.us')
+    expect(formatPhone(validPhoneStr)).toBe('558776895831@c.us')
+    expect(formatPhone(otherValidPhoneStr)).toBe('558753135930@c.us')
 })
