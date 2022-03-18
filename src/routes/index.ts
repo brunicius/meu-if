4f1b4593cabaa30cc8378f2db088a03907bfdda9
Bruno Vinicius <vruno.binicius@gmail.com>
2022-03-17 23:03:15 -0300

    Some work

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,36 @@
 import {Router} from 'express'
+import httpCodes from '../util/HttpCodes'
+import { isValidPhone, formatPhone } from '../validators/phone'
 import { app } from '../app'
 
 const router = Router()
 
 
 router.get('/', (req, res)=>{
-    app.messenger.sendMessage('558781615939@c.us', 'test')
-
     return res.json({
         success: true
+    })
+})
+router.get('/test', (req, res)=>{
+    let phoneNumber = req.query.phone?.toString().trim(),
+        message     = req.query.message?.toString().trim()
+
+    if (!phoneNumber || !message)
+        return res.status(httpCodes.BAD_REQUEST).json({
+            error: 'Insufficient data'
+        })
+
+    if (!isValidPhone(phoneNumber))
+        return res.status(httpCodes.BAD_REQUEST).json({
+            error: 'Invalid phone number'
+        })
+
+    let formattedPhoneNumber = formatPhone(phoneNumber)
+
+    app.messenger.sendMessage(formattedPhoneNumber, message)
+
+    return res.status(httpCodes.SUCCESS).json({
+        message: "Message sended"
     })
 })
 
